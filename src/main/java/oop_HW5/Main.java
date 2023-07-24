2dcae9519fac78af85366fcede4cdda07d561fb1
Ilya Dorofeev <dara321@yandex.ru>
2023-07-24 23:55:08 +0300

    Дз к 5 семинар ООП

---
+++ src/main/java/oop_HW5/Main.java
@@ -0,0 +1,25 @@
+package oop_HW5;
+
+import oop_HW5.controllers.GroupController;
+import oop_HW5.controllers.StudentController;
+import oop_HW5.controllers.TeacherController;
+import oop_HW5.models.Student;
+import oop_HW5.models.Teacher;
+import oop_HW5.repositories.AllUserRepository;
+
+import oop_HW5.services.GroupService;
+import oop_HW5.services.StudentService;
+import oop_HW5.services.TeacherService;
+import oop_HW5.view.AllUsersView;
+import oop_HW5.view.GroupView;
+import oop_HW5.view.SortType;
+
+import static oop_HW5.UniversityApp.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        UniversityApp universityApp = new UniversityApp();
+        universityApp.run();
+    }
+}
